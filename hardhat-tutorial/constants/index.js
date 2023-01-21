ba376fb240b524a84552633cf2473907b230bce5
cd <cleardevice@gmail.com>
2023-01-21 04:47:45 +0200

    init

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x786FE8DF5a8579C00Fa932e1e291fb3FC96dCdD7";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
